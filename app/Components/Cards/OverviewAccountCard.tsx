50315db4b339357f5aea693db4729c60de649d5d
unknown <nwosumick@gmail.com>
2026-02-18 17:03:12 +0100

    fix bug

--- app/Components/Cards/OverviewAccountCard.tsx
+++ app/Components/Cards/OverviewAccountCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { FiPhoneIncoming, FiPhoneMissed } from "react-icons/fi";
 
 type Props = {
@@ -13,7 +14,7 @@ type Props = {
 };
 
 const OverviewAccountCard = ({ account, isFirst = false }: Props) => {
-  // Incoming icon for Active Loans & Total Disbursed
+  // Incoming icon for Active Loans & Total Disturbed
   const showIncoming =
     account.title === "Active Loans" || account.title === "Total Disturbed";
 
@@ -29,17 +30,17 @@ const OverviewAccountCard = ({ account, isFirst = false }: Props) => {
       }`}
     >
       <div className="flex gap-2 mb-3 items-center">
-        <img
+        <Image
           src={`/images/item${account.id}.png`}
-          alt={account.title}
-          className="w-8 h-8"
+          alt="title"
+          width={32}
+          height={32}
         />
         <p className="text-sm">{account.title}</p>
       </div>
 
       <h2 className="text-xl font-semibold">{account.balance}</h2>
 
-      {/* Icons always visible */}
       <div className="flex gap-2 items-center text-xs mt-2">
         {showIncoming && <FiPhoneIncoming className="text-green-500" />}
         {showMissed && <FiPhoneMissed className="text-red-500" />}
